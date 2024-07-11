77298db2c31e45f6e66f79d3f47ef0461ac16a25
sumaiya <sumaiyayesmen556@gmail.com>
2024-07-11 20:30:19 +0600

    creating new footer

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Container from './Container'
+
+const Footer = () => {
+  return (
+    <section>
+      <Container>
+        hlww
+      </Container>
+    </section>
+  )
+}
+
+export default Footer
