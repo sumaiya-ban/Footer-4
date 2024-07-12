eb2b047531679a1999ccbdb529f9f82fa7e126ef
sumaiya <56853750+sumaiya-ban@users.noreply.github.com>
2024-07-12 20:14:04 +0600

    Update Container.jsx

--- src/Components/Container.jsx
+++ src/Components/Container.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Container = ({children}) => {
   return (
-    <div className='max-w-container m-auto'>
+    <div className='max-w-container m-auto lg:pt-0 pt-8 lg:px-0 px-4'>
       {children}
     </div>
   )
